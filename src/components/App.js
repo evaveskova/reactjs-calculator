4bc4d15431d3d66efe06a470cd4469e676f2b046
Eva Veskova Jackson <51458322+evaveskova@users.noreply.github.com>
2020-03-05 21:22:31 +0100

    create the button, button panel, and display components and update App.js

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Display from './display';
+import ButtonPanel from './button-panel';
+
+function App() {
+  return (
+    <div id="App">
+      <Display />
+      {' '}
+      <ButtonPanel />
+    </div>
+  );
+}
+
+export default App;
